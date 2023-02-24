44443fb290bb7ce0b824c7ff09635253f903f51c
tranquocbao0031 <tranquocbao0031@gmail.com>
2023-02-24 10:32:39 +0700

    bao lam

---
+++ Views/404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>404 Not Found</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" <script
+        src='main.js'>
+    </script>
+</head>
+
+<body>
+    <div class="container ny-5">
+        <h1 class="text-center mb-4">404 Not Found</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <p>Không thể tìm thấy trang được yêu cầu máy chủ trên này.Vui lòng kiểm tra URL hoặc liên hệ với quản
+                    trị viên.</p>
+                <center>
+                    <a href="?r=/" class="btn btn-primary">Quay về trang chủ</a>
+                </center>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com//ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
