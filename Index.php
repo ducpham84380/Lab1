94d8eefe1212d85f01f110b73373b4b2c32c1008
duc84380 <duc84380@gmail.com>
2023-02-24 19:15:32 +0700

    update Index

--- Index.php
+++ Index.php
@@ -1,1 +1,12 @@
-Laragon
+<?php
+require_once('Route.php');
+$r = $_GET['r'] ?? '/';
+if (isset($route[$r])) {
+    $controllerName = $routes[$r]['controller'];
+    $action = $routes[$r]['action'];
+    require_once('Controller/' . $controllerName . ' .php');
+    $controller = new $controllerName();
+    $controller->$action();
+} else {
+    require_once('View/404.php');
+}
