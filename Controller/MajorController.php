0a834fa7f744a3b5aab8920136775879c6b371e9
duc84380 <duc84380@gmail.com>
2023-02-24 19:32:32 +0700

    update Major

--- Controller/MajorController.php
+++ Controller/MajorController.php
@@ -1,8 +1,45 @@
 <?php
+require_once("Config/Database.php");
+require_once("Models/Registration.php");
 class MajorController
 {
+    private $model;
+
+    function __construct()
+    {
+        global $$pdo;
+        $this->model = new Registration($pdo);
+    }
+
     function index()
     {
         require_once("Views/Register.php");
     }
+
+    function add()
+    {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $hoten = $_POST['name'];
+            $lop = $_POST['class'];
+            $chuyennghanh = $_POST['major'];
+            $mssv = $_POST['studentID'];
+            $this->model->addStudent( $mssv, $hoten, $lop, $chuyennghanh);
+            header("Location: Index.php");
+        }
+    }
+
+    function getList(){
+        $danhSach = $this->model->getList();
+        require_once("Views/List.php");
+    }
+
+    function add()
+    {
+        if($_SERVER['REQUEST_METHOD'] == 'GET'){
+            $id = $_GET['id'];
+            $this->model->deleteStudent( $id);
+            header("Location: ?r=list");
+        }
+    }
+
 }
